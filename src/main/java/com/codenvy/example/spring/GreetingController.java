29e1469506193f40c9608af69c0acb399d0fa871
Paresh <sharma.paresh@gmail.com>
2015-07-15 20:57:02 +0000

    Second Commit

--- src/main/java/com/codenvy/example/spring/GreetingController.java
+++ src/main/java/com/codenvy/example/spring/GreetingController.java
@@ -16,7 +16,7 @@ public class GreetingController implements Controller
       String result = "";
       if (userName != null)
       {
-        result = "Hello, " + userName + "! Excellent Day has begun";
+        result = "Hello Dear, " + userName + "! Excellent Day has begun";
       }
 
       ModelAndView view = new ModelAndView("hello_view");
